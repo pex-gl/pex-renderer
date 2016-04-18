1aa115f002e10bd1c416b1a92e6486c28542055b
Marcin Ignac <marcin.ignac@gmail.com>
2016-04-18 21:37:06 +0100

    pex-render-to-cubemap added mip level selection

--- local_modules/pex-render-to-cubemap/index.js
+++ local_modules/pex-render-to-cubemap/index.js
@@ -2,12 +2,12 @@ var Mat4 = require('pex-math/Mat4');
 
 //Flipping up by -1 inspired by http://www.mbroecker.com/project_dynamic_cubemaps.html
 var sides = [
-    { eye: [0, 0, 0], target: [ 1, 0, 0], up: [0, -1, 0], color: [1, 0, 0, 1] },
-    { eye: [0, 0, 0], target: [-1, 0, 0], up: [0, -1, 0], color: [0, 0, 0, 1] },
-    { eye: [0, 0, 0], target: [0,  1, 0], up: [0, 0, 1], color: [0, 0, 0, 1] },
-    { eye: [0, 0, 0], target: [0, -1, 0], up: [0, 0, -1], color: [0, 0, 0, 1] },
-    { eye: [0, 0, 0], target: [0, 0,  1], up: [0, -1, 0], color: [0, 0, 0, 1] },
-    { eye: [0, 0, 0], target: [0, 0, -1], up: [0, -1, 0], color: [0, 0, 0, 1] },
+    { eye: [0, 0, 0], target: [ 1, 0, 0], up: [0, -1, 0] },
+    { eye: [0, 0, 0], target: [-1, 0, 0], up: [0, -1, 0] },
+    { eye: [0, 0, 0], target: [0,  1, 0], up: [0,  0, 1] },
+    { eye: [0, 0, 0], target: [0, -1, 0], up: [0,  0,-1] },
+    { eye: [0, 0, 0], target: [0, 0,  1], up: [0, -1, 0] },
+    { eye: [0, 0, 0], target: [0, 0, -1], up: [0, -1, 0] },
 ];
 
 var fbo = null;
@@ -22,14 +22,15 @@ function renderToCubemap(ctx, cubemap, drawScene, level) {
         viewMatrix = Mat4.create();
     }
 
+    var levelScale = 1.0 / Math.pow(2.0, level);
+
     ctx.pushState(ctx.VIEWPORT_BIT | ctx.FRAMEBUFFER_BIT | ctx.MATRIX_PROJECTION_BIT | ctx.MATRIX_VIEW_BIT | ctx.COLOR_BIT);
-    ctx.setViewport(0, 0, cubemap.getWidth(), cubemap.getHeight());
+    ctx.setViewport(0, 0, cubemap.getWidth()*levelScale, cubemap.getHeight()*levelScale);
     ctx.bindFramebuffer(fbo);
     ctx.setProjectionMatrix(projectionMatrix);
     sides.forEach(function(side, sideIndex) {
         fbo.setColorAttachment(0, ctx.TEXTURE_CUBE_MAP_POSITIVE_X + sideIndex, cubemap.getHandle(), level);
-        ctx.setClearColor(side.color[0], side.color[1], side.color[2], 1);
-        //ctx.setClearColor(0, 0, 0, 1);
+        ctx.setClearColor(0, 0, 0, 0);
         ctx.clear(ctx.COLOR_BIT | ctx.DEPTH_BIT);
         Mat4.lookAt(viewMatrix, side.eye, side.target, side.up);
         ctx.setViewMatrix(viewMatrix);
