8f17901c60a9a74e5f9cd3c5ab55d3dba061e9f4
Marcin Ignac <marcin.ignac@gmail.com>
2016-06-21 14:38:37 +0100

    CommandQueue fixed caching of previous shader values

--- CommandQueue.js
+++ CommandQueue.js
@@ -1,7 +1,23 @@
+function copyArray (a, na) {
+  na = na || []
+  for (var i = 0; i < a.length; i++) {
+    na[i] = a[i]
+  }
+  return na
+}
+
+function arrayEquals (a, b) {
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+  return true
+}
+
 function CommandQueue (ctx) {
   this._ctx = ctx
-  this._commands = []
-  this._prevUniforms = {}
+  this._commands = 0
   this._prevProgram = null
   this._prevTextures = []
 
@@ -11,7 +27,7 @@ function CommandQueue (ctx) {
 
 var ID = 0
 
-var updatedUniforms = 0
+var updatedUniforms = { }
 var createdCommands = 0
 
 CommandQueue.prototype.getContext = function () {
@@ -69,49 +85,44 @@ function applyUniforms (ctx, program, uniforms, prevUniforms, prevTextures, debu
     var value = uniforms[uniformName]
     if (debug) {
       if (value === null || value === undefined) {
-        if (program._uniforms[uniformName]) {
-          throw new Error('Null uniform value for ' + uniformName + ' in PBRMaterial')
-        } else {
-          // console.log('Unnecessary uniform', uniformName)
-          continue
-        }
+        throw new Error('Null uniform value for ' + uniformName + ' in PBRMaterial')
       }
     }
-    // if (value.getTarget && (value.getTarget() === ctx.TEXTURE_2D || value.getTarget() === ctx.TEXTURE_CUBE_MAP)) {
-    if (value.getTarget) {
+    if (value.getTarget) { // it's a texture
       if (prevTextures[textureOffset] !== value) {
         ctx.bindTexture(value, textureOffset)
         prevTextures[textureOffset] = value
       }
       value = textureOffset++
     }
+
     var shouldUpdate = false
-    if (!prevUniforms) {
+    var prevValue = prevUniforms[uniformName]
+    var isArray = Array.isArray(value)
+
+    if (prevValue === undefined) {
       shouldUpdate = true
-    } else {
-      var prevValue = prevUniforms[uniformName]
-      if (!prevValue) {
+    } else if (isArray) {
+      if (!arrayEquals(value, prevValue)) {
         shouldUpdate = true
-      } else if (value !== prevValue) {
-        shouldUpdate = true
-      } else {
-        if (Array.isArray(value)) {
-          shouldUpdate = true
-          // for (var i = 0; i < value.length; i++) {
-            // if (value[i] !== prevValue[i]) {
-              // shouldUpdate = true
-              // break
-            // }
-          // }
-        }
       }
+    } else if (value !== prevValue) {
+      shouldUpdate = true
     }
+
     if (shouldUpdate) {
+      prevUniforms[uniformName] = isArray ? copyArray(value, prevUniforms[uniformName]) : value
       program.setUniform(uniformName, value)
-      updatedUniforms++
+      updatedUniforms[uniformName] = (updatedUniforms[uniformName] || 0) + 1
     }
   }
   return textureOffset
+}
+
+function debugObject (cmd) {
+  for (var prop in cmd) {
+    console.log('  ', prop, '' + cmd[prop])
+  }
 }
 
 // cmd - a command to submit to the queue, the immediate execution is not guaranteed and will
@@ -120,8 +131,13 @@ function applyUniforms (ctx, program, uniforms, prevUniforms, prevTextures, debu
 // subContextCallback - all submit calls within this callback will
 // inherit defaults from the currently executing cmd
 CommandQueue.prototype.submit = function (cmd, opts, subCommanDraw) {
-  this._commands.push([cmd, opts, subCommanDraw])
-  // if (this.debug) console.log('submit', cmd._type, cmd._id)
+  this._commands++// .push([cmd, opts, subCommanDraw])
+  // if (this.debug) console.log('submit', cmd._type, cmd._id, cmd.name || '')
+
+  if (this.debug && cmd.name) {
+    console.log('prev uniforms')
+    debugObject(this._prevUniforms)
+  }
 
   if (!cmd._type) {
     if (this.debug) console.log(cmd.toString())
@@ -178,13 +194,14 @@ CommandQueue.prototype.submit = function (cmd, opts, subCommanDraw) {
     if (this._prevProgram !== cmd.program) {
       ctx.bindProgram(cmd.program)
       this._prevProgram = cmd.program
-      this._prevUniforms = null
+      if (!cmd.program._prevUniforms) {
+        cmd.program._prevUniforms = {}
+      }
     }
   }
 
   if (cmd.uniforms !== undefined) {
-    applyUniforms(ctx, cmd.program, cmd.uniforms, this._prevUniforms, this._prevTextures, this.debug)
-    this._prevUniforms = cmd.uniforms
+    applyUniforms(ctx, cmd.program, cmd.uniforms, cmd.program._prevUniforms, this._prevTextures, this.debug)
   }
 
   if (cmd.projectionMatrix !== undefined) {
@@ -253,13 +270,15 @@ CommandQueue.prototype.submit = function (cmd, opts, subCommanDraw) {
 // force execute of all commands in the queue
 CommandQueue.prototype.flush = function () {
   if (this.profile) {
-    console.log('CommandQueue', 'numCommands', this._commands.length, 'updatedUniforms', updatedUniforms, 'createdCommands', createdCommands)
+    console.log('CommandQueue', 'numCommands', this._commands, 'updatedUniforms', Object.keys(updatedUniforms).length, 'createdCommands', createdCommands)
   }
   this._prevProgram = null
-  this._prevUniforms = null
   this._prevTextures.length = 0
-  this._commands.length = 0
-  updatedUniforms = 0
+  this._commands = 0
+  if (this.profile) {
+    console.log('CommandQueue', 'updatedUniforms', updatedUniforms)
+  }
+  updatedUniforms = {}
   createdCommands = 0
 }
 
