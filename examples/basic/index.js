074ff4ee190a164cf949c30c05e558e2e640ac73
Marcin Ignac <marcin.ignac@gmail.com>
2017-05-04 11:10:54 +0100

    Add point lights to the basic example

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -3,7 +3,7 @@ const Quat = require('pex-math/Quat')
 const Vec3 = require('pex-math/Vec3')
 const createCamera = require('pex-cam/perspective')
 const createOrbiter = require('pex-cam/orbiter')
-const Renderer = require('../../Renderer')
+const createRenderer = require('../../')
 const createRoundedCube = require('primitive-rounded-cube')
 const createSphere = require('primitive-sphere')
 const createCapsule = require('primitive-capsule')
@@ -13,6 +13,12 @@ const random = require('pex-random')
 const createContext = require('pex-context')
 const io = require('pex-io')
 const isBrowser = require('is-browser')
+const dragon = require('stanford-dragon/3')
+const normals = require('angle-normals')
+const centerAndNormalize = require('geom-center-and-normalize')
+dragon.positions = centerAndNormalize(dragon.positions).map((v) => Vec3.scale(v, 5))
+dragon.normals = normals(dragon.cells, dragon.positions)
+dragon.uvs = dragon.positions.map(() => [0, 0])
 
 const ctx = createContext()
 ctx.gl.getExtension('EXT_shader_texture_lod')
@@ -35,23 +41,35 @@ const State = {
 
 random.seed(10)
 
-const renderer = new Renderer(ctx, ctx.gl.drawingBufferWidth, ctx.gl.drawingBufferHeight)
+const renderer = createRenderer({ ctx: ctx })
 
 const gui = createGUI(ctx)
-gui.addHeader('Settings')
-gui.addParam('New PBR', renderer._state, 'useNewPBR')
 gui.addHeader('Sun')
 gui.addParam('Sun Elevation', State, 'elevation', { min: -90, max: 180 }, updateSunPosition)
 gui.addParam('Sun Azimuth', State, 'azimuth', { min: -180, max: 180 }, updateSunPosition)
-gui.addTexture2D('Skybox', renderer._skyEnvMapTex.texture)
 
 function updateSunPosition () {
   Mat4.setRotation(State.elevationMat, State.elevation / 180 * Math.PI, [0, 0, 1])
   Mat4.setRotation(State.rotationMat, State.azimuth / 180 * Math.PI, [0, 1, 0])
 
-  Vec3.set3(renderer._state.sunPosition, 1, 0, 0)
-  Vec3.multMat4(renderer._state.sunPosition, State.elevationMat)
-  Vec3.multMat4(renderer._state.sunPosition, State.rotationMat)
+  Vec3.set3(State.sunPosition, 10, 0, 0)
+  Vec3.multMat4(State.sunPosition, State.elevationMat)
+  Vec3.multMat4(State.sunPosition, State.rotationMat)
+
+  if (State.sun) {
+    var sunDir = State.sun.direction
+    Vec3.set(sunDir, [0, 0, 0])
+    Vec3.sub(sunDir, State.sunPosition)
+    State.sun.set({ direction: sunDir })
+  }
+
+  if (State.skybox) {
+    State.skybox.set({ sunPosition: State.sunPosition })
+  }
+
+  if (State.reflectionProbe) {
+    State.reflectionProbe.dirty = true // FIXME: hack
+  }
 }
 
 // gui.addParam('Exposure', renderer._state, 'exposure', { min: 0.01, max: 5 })
@@ -62,103 +80,155 @@ function updateSunPosition () {
 
 function initCamera () {
   const camera = createCamera({
-    fov: 45,
+    fov: Math.PI / 3,
     aspect: ctx.gl.drawingBufferWidth / ctx.gl.drawingBufferHeight,
     position: [0, 3, 8],
     target: [0, 0, 0],
     near: 0.1,
     far: 100
   })
-
-  var cameraNode = renderer.createNode({
-    camera: camera
-  })
-  renderer.add(cameraNode)
-
   createOrbiter({ camera: camera })
-}
 
-function buildMesh (geometry, primitiveType) {
-  if (primitiveType) throw new Error('primitiveType not supported yet')
-  return {
-    attributes: {
-      aPosition: ctx.vertexBuffer(geometry.positions),
-      aTexCoord0: ctx.vertexBuffer(geometry.uvs),
-      aNormal: ctx.vertexBuffer(geometry.normals)
-    },
-    indices: ctx.indexBuffer(geometry.cells)
-  }
+  renderer.entity([
+    renderer.camera({ camera: camera })
+  ])
 }
 
 function initMeshes () {
-  const roundedCubeMesh = buildMesh(createRoundedCube(1, 1, 1, 20, 20, 20, 0.2))
-  const capsuleMesh = buildMesh(createCapsule(0.3))
-  const sphereMesh = buildMesh(createSphere(0.5))
-  const meshes = [capsuleMesh, roundedCubeMesh, sphereMesh]
-  const nodes = []
-  for (let i = 0; i < meshes.length; i++) {
-    const mesh = meshes[i]
-    let materialIndex = 0
-    for (var j = -5; j <= 5; j += 2, materialIndex++) {
+  const groundCube = createRoundedCube(1, 1, 1, 20, 20, 20, 0.01)
+  const roundedCube = createRoundedCube(1, 1, 1, 20, 20, 20, 0.2)
+  const capsule = createCapsule(0.3)
+  const sphere = createSphere(0.5)
+  const geometries = [capsule, roundedCube, sphere]
+  const entities = []
+
+  renderer.entity([
+    renderer.transform({
+      position: [0, -2.2, 0],
+      scale: [20, 0.2, 7]
+    }),
+    renderer.geometry(groundCube),
+    renderer.material({
+      baseColor: [0.15, 0.15, 0.2, 1.0],
+      roughness: 1,
+      metallic: 0
+    })
+  ])
+
+  const dragonEnt = renderer.entity([
+    renderer.transform({
+      position: [0, -0.4, 2],
+    }),
+    renderer.geometry(dragon),
+    renderer.material({
+      baseColor: [0.8, 0.8, 0.8, 1.0],
+      roughness: 1,
+      metallic: 0
+    })
+  ])
+  entities.push(dragonEnt)
+
+  let materialIndex = 0
+  for (let j = -5; j <= 5; j += 2) {
+    for (let i = 0; i < geometries.length; i++) {
+      const geom = geometries[i]
       const x = j
       const y = 1.5 * (1 - i)
       const z = 0
-      const node = renderer.createNode({
-        mesh: mesh,
-        position: [x, y, z],
-        material: {
-          baseColor: [0.8, 0.1, 0.1, 1],
-          baseColorMap: State.materials[materialIndex].baseColorTex,
-          rougness: 0.5, // (k + 5) / 10,
-          // roughnessMap: State.materials[materialIndex].roughnessTex,
-          metallic: 0.01,// (j + 5) / 10
-          // metallicMap: State.materials[materialIndex].metallicTex,
-          normalMap: State.materials[materialIndex].normalTex
-        }
-      })
-      nodes.push(node)
-      renderer.add(node)
+      const entity = renderer.entity([
+        renderer.transform({
+          position: [x, y, z]
+        }),
+        renderer.geometry(geom),
+        renderer.material({
+          baseColor: [0.9, 0.9, 0.9, 1],
+          baseColorMap: materialIndex ? State.materials[materialIndex].baseColorTex : null,
+          roughness: (j + 5) / 10,
+          roughnessMap: materialIndex ? State.materials[materialIndex].roughnessTex : null,
+          metallic: 0.0, // 0.01, // (j + 5) / 10
+          metallicMap: materialIndex ? State.materials[materialIndex].metallicTex : null,
+          normalMap: materialIndex ? State.materials[materialIndex].normalTex : null
+        })
+      ])
+      entities.push(entity)
+      break
     }
+    materialIndex = (materialIndex + 1) % State.materials.length
+    break
   }
   gui.addHeader('Material')
   gui.addParam('Roughness', State, 'roughness', {}, () => {
-    nodes.forEach((node) => { node.data.material.roughness = State.roughness })
+    entities.forEach((entity) => { entity.getComponent('Material').set({ roughness: State.roughness }) })
   })
   gui.addParam('Metallic', State, 'metallic', {}, () => {
-    nodes.forEach((node) => { node.data.material.metallic = State.metallic })
+    entities.forEach((entity) => { entity.getComponent('Material').set({ metallic :State.metallic }) })
   })
   gui.addParam('Base Color', State, 'baseColor', { type: 'color' }, () => {
-    nodes.forEach((node) => { node.data.material.baseColor = State.baseColor })
+    entities.forEach((entity) => { entity.getComponent('Material').set({ baseColor: State.baseColor }) })
   })
 }
 
-function initLights () {
-  var sunDir = Vec3.normalize([1, -1, 1])
-  var sunLightNode = renderer.createNode({
-    light: {
-      type: 'directional',
-      color: [1, 1, 0, 1],
-      direction: sunDir
-    }
+function initSky (panorama) {
+  const sun = State.sun = renderer.directionalLight({
+    direction: Vec3.sub(Vec3.create(), State.sunPosition),
+    color: [1, 1, 0.95, 1],
+    intensity: 10
   })
-  // renderer.add(sunLightNode)
 
-  gui.addTexture2D('Shadow Map', renderer._sunLightNode.data.light._shadowMap)
-  const sphereMesh = buildMesh(createSphere(0.2))
-  var pointLight = renderer.createNode({
-    mesh: sphereMesh,
-    position: [2, 2, 2],
-    material: {
-      baseColor: [1, 0, 0, 1],
-      emissiveColor: [1, 0, 0, 1]
-    },
-    light: {
-      type: 'point',
-      position: [2, 2, 2],
-      color: [1, 0, 0, 1],
-      radius: 50
-    }
+  const skybox = State.skybox = renderer.skybox({
+    sunPosition: State.sunPosition
   })
+
+  // currently this also includes light probe functionality
+  const reflectionProbe = State.reflectionProbe = renderer.reflectionProbe({
+    origin: [0, 0, 0],
+    size: [10, 10, 10],
+    boxProjection: false
+  })
+
+  renderer.entity([ sun ])
+  renderer.entity([ skybox ])
+  renderer.entity([ reflectionProbe ])
+}
+
+function initLights () {
+  const pointLight1 = renderer.entity([
+    renderer.geometry(createSphere(0.2)),
+    renderer.material({
+      baseColor: [0, 0, 0, 1],
+      emissiveColor: [1, 0, 0, 1]
+    }),
+    renderer.transform({
+      position: [2, 2, 2]
+    }),
+    renderer.pointLight({
+      color: [1, 0, 0, 1],
+      intensity: 3,
+      radius: 10
+    })
+  ])
+
+  const pointLight2 = renderer.entity([
+    renderer.geometry(createSphere(0.2)),
+    renderer.material({
+      baseColor: [0, 0, 0, 1],
+      emissiveColor: [0, 0.5, 1, 1]
+    }),
+    renderer.transform({
+      position: [-2, 2, 2]
+    }),
+    renderer.pointLight({
+      color: [0, 0.5, 1, 1],
+      intensity: 3,
+      radius: 10
+    })
+  ])
+
+  gui.addParam('Light 1 Pos', pointLight1.transform, 'position', { min: -5, max: 5 }, (value) => {
+    pointLight1.transform.set({ position: value })
+  })
+
+  /*
   // renderer.add(pointLight)
 
   var areaLightMesh = buildMesh(createCube(1))
@@ -187,58 +257,14 @@ function initLights () {
   gui.addParam('AreaLight Intensity', areaLightNode.data.light, 'intensity', { min: 0, max: 5 })
   gui.addParam('AreaLight', areaLightNode.data.light, 'color', { type: 'color' })
   renderer.add(areaLightNode)
+  */
 }
 
-function imageFromFile (file, options) {
-  let tex = ctx.texture2D({ width: 1, height: 1})
-  io.loadImage(file, function (err, image) {
-    if (err) console.log(err)
-    ctx.update(tex, { data: image, wrap: ctx.Wrap.Repeat, flipY: true })
-    // tex(Object.assign(canvasToPixels(image), {
-      // min: 'mipmap',
-      // mag: 'linear',
-      // wrap: 'repeat'
-    // }))
-  }, true)
-  return tex
-}
-
-function initMaterials () {
-  const ASSETS_DIR = isBrowser ? 'http://localhost/assets' : '/Users/vorg/Workspace/assets'
-  const baseColorTextures = [
-    ASSETS_DIR + '/textures/gametextures_old_met/Brick_DustyRedSeattle_PBR/Brick_DustyRedSeattle_Base_Color.png',
-    ASSETS_DIR + '/textures/gametextures_old_met/Metal_FloorPanelModularPainted_pbr/Metal_FloorPanelModularPainted_Base_Color.png',
-    ASSETS_DIR + '/textures/gametextures_old_met/Metal_MatiasSciFiCieling/Metal_MatiasSciFiCieling_Base_Color.png',
-    ASSETS_DIR + '/textures/gametextures_old_met/Metal_PaintedWeatheredMetal/Metal_PaintedWeatheredMetal_Base_Color.png',
-    ASSETS_DIR + '/textures/gametextures_old_met/Metal_SciFiCompartmentPanels_PBR/Metal_SciFiCompartmentPanels_Base_Color.png',
-    ASSETS_DIR + '/textures/gametextures_old_met/Metal_SciFiFlatPlatingSquare_pbr/Metal_SciFiFlatPlatingSquare_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Metal_ScifiTrimPieces_PBR/Metal_ScifiTrimPieces_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Metal_SteelOxidized_PBR/Metal_SteelOxidized_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Misc_BlackLeather_PBR/Misc_BlackLeather_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Vorg_Var_Plastic/Vorg_Var_Plastic_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Vorg_White_Base/Vorg_White_Base_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Wood_LongWideFloorPlanks/Wood_LongWideFloorPlanks_Base_Color.png',
-		// ASSETS_DIR + '/textures/gametextures_old_met/Wood_OakBaseAged/Wood_OakBaseAged_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/Wood_RailwaySleeper/Wood_RailwaySleeper_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/ground_lavaflow_pbr/ground_lavaflow_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/metal_DiamondPlateFloor_PBR/metal_DiamondPlateFloor_Base_Color.png',
-    // ASSETS_DIR + '/textures/gametextures_old_met/tile_disgustingtilev2_PBR/tile_disgustingtilev2_Base_Color.png'
-  ]
-  for (let i = 0; i < baseColorTextures.length; i++) {
-    const mat = {}
-    mat.baseColorTex = imageFromFile(baseColorTextures[i], { flip: false, mipmap: true, repeat: true })
-    mat.normalTex = imageFromFile(baseColorTextures[i].replace('_Base_Color.', '_Normal.'), { flip: false, mipmap: true, repeat: true })
-    mat.roughnessTex = imageFromFile(baseColorTextures[i].replace('_Base_Color.', '_Roughness.'), { flip: false, mipmap: true, repeat: true })
-    mat.metallicTex = imageFromFile(baseColorTextures[i].replace('_Base_Color.', '_Metallic.'), { flip: false, mipmap: true, repeat: true })
-    State.materials.push(mat)
-  }
-}
-
-initMaterials()
 
 initCamera()
 initMeshes()
-// initLights()
+initSky()
+initLights()
 
 let frameNumber = 0
 let debugOnce = false
@@ -250,7 +276,6 @@ window.addEventListener('keydown', (e) => {
 updateSunPosition()
 
 ctx.frame(() => {
-  // ctx.debug(frameNumber++ == 1)
   ctx.debug(debugOnce)
   debugOnce = false
   renderer.draw()
