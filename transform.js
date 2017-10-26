3ba43ed7adeafc9035469a4b00cbc79eca1c33a8
Marcin Ignac <marcin.ignac@gmail.com>
2017-10-26 10:52:42 +0100

    Simplify world bounding box udates

--- transform.js
+++ transform.js
@@ -3,15 +3,8 @@ const Vec3 = require('pex-math/Vec3')
 const Mat4 = require('pex-math/Mat4')
 const AABB = require('pex-geom/AABB')
 
-// TODO ?
-function multVec3 (a, b) {
-  a[0] *= b[0]
-  a[1] *= b[1]
-  a[2] *= b[2]
-}
-
 // TODO remove, should in AABB
-function emptyABBB (a) {
+function emptyAABB (a) {
   a[0][0] = Infinity
   a[0][1] = Infinity
   a[0][2] = Infinity
@@ -39,7 +32,6 @@ function Transform (opts) {
   this.type = 'Transform'
   this.entity = null
   this.changed = new Signal()
-  this.dirty = true
   this.position = [0, 0, 0]
   this.worldPosition = [0, 0, 0]
   this.rotation = [0, 0, 0, 1]
@@ -59,15 +51,6 @@ function Transform (opts) {
 
 Transform.prototype.init = function (entity) {
   this.entity = entity
-  this.geometry = entity.getComponent('Geometry')
-  if (this.geometry) {
-    this.geometry.changed.add((prop) => {
-      if (prop === 'bounds') {
-        // TODO: not needed? transform is live and updates every frame?
-        this.dirty = true
-      }
-    })
-  }
 }
 
 Transform.prototype.set = function (opts) {
@@ -81,7 +64,6 @@ Transform.prototype.set = function (opts) {
   }
   Object.assign(this, opts)
   Object.keys(opts).forEach((prop) => this.changed.dispatch(prop))
-  this.dirty = true
 }
 
 Transform.prototype.update = function () {
@@ -102,33 +84,26 @@ Transform.prototype.update = function () {
     Mat4.mult(this.modelMatrix, p.localModelMatrix)
   })
 
-  emptyABBB(this.bounds)
-  if (this.geometry) {
-    AABB.set(this.bounds, this.geometry.bounds)
+  emptyAABB(this.bounds)
+  const geom = this.entity.getComponent('Geometry')
+  if (geom) {
+    AABB.set(this.bounds, geom.bounds)
   }
 }
 
 Transform.prototype.afterUpdate = function () {
-  this.children.forEach((child) => {
-    let childBounds = AABB.copy(child.bounds)
-    multVec3(childBounds[0], child.scale)
-    multVec3(childBounds[1], child.scale)
-    Vec3.add(childBounds[0], child.position)
-    Vec3.add(childBounds[1], child.position)
-    if (child.matrix) {
-      Vec3.multMat4(childBounds[0], child.matrix)
-      Vec3.multMat4(childBounds[1], child.matrix)
-    }
-    AABB.includeAABB(this.bounds, childBounds)
-  })
-
-  if (AABB.isEmpty(this.bounds)) {
-    AABB.includePoint(this.bounds, [0, 0, 0])
+  if (!AABB.isEmpty(this.bounds)) {
+    const points = aabbToPoints(this.bounds)
+    const pointsInWorldSpace = points.map((p) => Vec3.multMat4(Vec3.copy(p), this.modelMatrix))
+    this.worldBounds = AABB.fromPoints(pointsInWorldSpace)
+  } else {
+    emptyAABB(this.worldBounds)
   }
-
-  const points = aabbToPoints(this.bounds)
-  const pointsInWorldSpace = points.map((p) => Vec3.multMat4(Vec3.copy(p), this.modelMatrix))
-  this.worldBounds = AABB.fromPoints(pointsInWorldSpace)
+  this.children.forEach((child) => {
+    if (!AABB.isEmpty(child.worldBounds)) {
+      AABB.includeAABB(this.worldBounds, child.worldBounds)
+    }
+  })
   Vec3.scale(this.worldPosition, 0)
   Vec3.multMat4(this.worldPosition, this.modelMatrix)
 }
