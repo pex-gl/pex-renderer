3c73556ee3082cc14dd42f152dc741b633116464
Marcin Ignac <marcin.ignac@gmail.com>
2017-11-25 22:39:44 +0000

    Add gamma to filmic tonemapping
    So it has the same api as reinhard and uncharted2

--- local_modules/glsl-tonemap-filmic/index.glsl
+++ local_modules/glsl-tonemap-filmic/index.glsl
@@ -1,7 +1,8 @@
 //Based on Filmic Tonemapping Operators http://filmicgames.com/archives/75
 vec3 tonemapFilmic(vec3 color) {
     vec3 x = max(vec3(0.0), color - 0.004);
-    return (x * (6.2 * x + 0.5)) / (x * (6.2 * x + 1.7) + 0.06);
+    vec3 result = (x * (6.2 * x + 0.5)) / (x * (6.2 * x + 1.7) + 0.06);
+    return pow(result, vec3(2.2));
 }
 
 #pragma glslify: export(tonemapFilmic)
