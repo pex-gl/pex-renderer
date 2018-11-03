96d47f80c234e09b453b10381e8b96b9fc7b5779
Marcin Ignac <marcin.ignac@gmail.com>
2018-11-03 15:16:45 +0000

    Skip empty geometries

--- index.js
+++ index.js
@@ -628,7 +628,14 @@ Renderer.prototype.drawMeshes = function (camera, shadowMapping, shadowMappingLi
     }
     const geometry = geometries[i]
     const transform = geometry.entity.transform
-    if (!transform.enabled) continue
+    if (!transform.enabled) {
+			continue
+		}
+
+    // don't draw uninitialized geometries
+		if (!geometry._attributes.aPosition) {
+		  continue
+		}
     const material = geometry.entity.getComponent('Material')
     if (material.blend && shadowMapping) {
       continue
