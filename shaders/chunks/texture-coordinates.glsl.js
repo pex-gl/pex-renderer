ba7c878963a869603860f647895828a337080b92
Damien Seguin <dmnsgn@gmail.com>
2019-03-29 10:35:13 +0000

    Clean texture-coordinates

--- shaders/chunks/texture-coordinates.glsl.js
+++ shaders/chunks/texture-coordinates.glsl.js
@@ -1,5 +1,5 @@
 module.exports = /* glsl */`
-vec2 getTextureCoordinates(const in PBRData data, in int index) {
+vec2 getTextureCoordinates(in PBRData data, in int index) {
   #ifdef USE_TEXCOORD_1
     if (index == 1) return data.texCoord1;
   #endif
