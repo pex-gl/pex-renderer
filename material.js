199eb10667e588fa78f02247ee1d75b8912c1e1f
Damien Seguin <dmnsgn@gmail.com>
2019-04-29 09:46:28 +0100

    Use mat3.mult and mat2x3 TRS for material

--- material.js
+++ material.js
@@ -1,54 +1,9 @@
 const Signal = require('signals')
+const { mat3, mat2x3 } = require('pex-math')
 
 let MaterialID = 0
 
-// TODO: add to pex-math
-function mat3Multiply(a, b) {
-  let a00 = a[0], a01 = a[1], a02 = a[2];
-  let a10 = a[3], a11 = a[4], a12 = a[5];
-  let a20 = a[6], a21 = a[7], a22 = a[8];
-
-  let b00 = b[0], b01 = b[1], b02 = b[2];
-  let b10 = b[3], b11 = b[4], b12 = b[5];
-  let b20 = b[6], b21 = b[7], b22 = b[8];
-
-  a[0] = b00 * a00 + b01 * a10 + b02 * a20;
-  a[1] = b00 * a01 + b01 * a11 + b02 * a21;
-  a[2] = b00 * a02 + b01 * a12 + b02 * a22;
-  a[3] = b10 * a00 + b11 * a10 + b12 * a20;
-  a[4] = b10 * a01 + b11 * a11 + b12 * a21;
-  a[5] = b10 * a02 + b11 * a12 + b12 * a22;
-  a[6] = b20 * a00 + b21 * a10 + b22 * a20;
-  a[7] = b20 * a01 + b21 * a11 + b22 * a21;
-  a[8] = b20 * a02 + b21 * a12 + b22 * a22;
-
-  return a;
-}
-
-function mat3FromTranslationRotationScale(translation, rotation, scale) {
-  const c = Math.cos(rotation)
-  const s = Math.sin(rotation)
-
-  return mat3Multiply(
-    mat3Multiply(
-      [
-        1, 0, 0,
-        0, 1, 0,
-        translation[0], translation[1], 1
-      ],
-      [
-        c, s, 0,
-        -s, c, 0,
-        0, 0, 1
-      ]
-    ),
-    [
-      scale[0], 0, 0,
-      0, scale[1], 0,
-      0, 0, 1
-    ]
-  );
-}
+const tempMat2x3 = mat2x3.create()
 
 const MATERIAL_MAPS = [
   'baseColorMap',
@@ -146,10 +101,16 @@ Material.prototype.set = function (opts) {
     for (let i = 0; i < mapKeys.length; i++) {
       const map = this[mapKeys[i]]
       if (map && map.texture) {
-        map.texCoordTransformMatrix = mat3FromTranslationRotationScale(
-          map.offset || [0, 0],
-          -map.rotation || 0,
-          map.scale || [1, 1]
+        mat2x3.identity(tempMat2x3)
+        mat2x3.translate(tempMat2x3, map.offset || [0, 0])
+        mat2x3.rotate(tempMat2x3, -map.rotation || 0)
+        mat2x3.scale(tempMat2x3, map.scale || [1, 1])
+
+        map.texCoordTransformMatrix = mat3.fromMat2x3(
+          map.texCoordTransformMatrix
+            ? mat3.identity(map.texCoordTransformMatrix)
+            : mat3.create(),
+          tempMat2x3
         )
       }
     }
