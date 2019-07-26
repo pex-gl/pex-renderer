0c6928c5a119eb84fdf3508aa546cd6dfec2d021
Simon <simonharris1992@gmail.com>
2019-07-26 17:52:31 +0100

    Add read depth fragment to dof shader

--- shaders/post-processing/dof.frag.js
+++ shaders/post-processing/dof.frag.js
@@ -1,3 +1,4 @@
+const SHADERS = require('../chunks/index.js')
 module.exports = /* glsl */ `
 precision highp float;
 
@@ -16,12 +17,7 @@ const float MAX_BLUR_SIZE = 20.0;
 const float RAD_SCALE = 0.5; // Smaller = nicer blur, larger = faster
 
 
-float readDepth(const in sampler2D depthMap, const in vec2 coord, const in float near, const in float far) {
-    float z_b = texture2D(depthMap, coord).r;
-    float z_n = 2.0 * z_b - 1.0;
-    float z_e = 2.0 * near * far / (far + near - z_n * (far - near));
-    return z_e;
-}
+${SHADERS.depthRead}
 
 float getBlurSize(float depth, float focusPoint, float focusScale)
 {
