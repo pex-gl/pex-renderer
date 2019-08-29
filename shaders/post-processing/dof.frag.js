9a65166b086fc63c6cdb30ebfe8fd57b8bf6cd32
Marcin Ignac <marcin.ignac@gmail.com>
2019-08-29 16:05:21 +0100

    Implement aperture based Circle of Confusion for DOF

--- shaders/post-processing/dof.frag.js
+++ shaders/post-processing/dof.frag.js
@@ -11,6 +11,8 @@ uniform float uFar; // Far plane
 uniform float uNear;
 uniform float uFocusDistance;
 uniform float uAperture;
+uniform float uFocalLength;
+uniform bool uDOFDebug;
 
 const float GOLDEN_ANGLE = 2.39996323; 
 const float MAX_BLUR_SIZE = 20.0; 
@@ -19,23 +21,41 @@ const float RAD_SCALE = 0.5; // Smaller = nicer blur, larger = faster
 
 ${SHADERS.depthRead}
 
-float getBlurSize(float depth, float focusPoint, float focusScale) {
-    float coc = clamp((1.0 / focusPoint - 1.0 / depth) * focusScale, -1.0, 1.0);
+float getCoCSize(float depth, float focusDistance, float maxCoC) {
+    float coc = (1.0 - focusDistance / depth) * maxCoC;
+
+    float sensorHeight = 24.0; // mm
+    coc = coc / sensorHeight;
     return abs(coc) * MAX_BLUR_SIZE;
 }
 
-vec3 depthOfField(vec2 texCoord, float focusPoint, float focusScale) {
-    float centerDepth = readDepth(depthMap, texCoord,uNear,uFar);
-    float centerSize = getBlurSize(centerDepth, focusPoint, focusScale);
-    //return vec3(centerSize/2.0);
+vec3 depthOfField(vec2 texCoord, float focusDistance, float maxCoC) {
+    // TODO: is this in meters?
+    float centerDepth = readDepth(depthMap, texCoord,uNear,uFar) / 1000.0;
+    float centerSize = getCoCSize(centerDepth, focusDistance, maxCoC);
+
+
+    if (uDOFDebug && texCoord.x > 0.5) {
+      float coc = (1.0 - focusDistance / centerDepth) * maxCoC;
+      float sensorHeight = 24.0; // mm
+      coc = coc / sensorHeight;
+      coc *= MAX_BLUR_SIZE;
+
+      float c = abs(coc);
+      c = c / (1.0 + c); // tonemapping to avoid burning the color
+      c = pow(c, 2.2); // gamma to linear
+
+      if (coc > 0.0) return vec3(c, 0.0, 0.0);
+      else return vec3(0.0, 0.0, c);
+    }
     vec3 color = texture2D(image, vTexCoord0).rgb;
     float tot = 1.0;
     float radius = RAD_SCALE;
     for (float ang = 0.0; ang < 180.0; ang += GOLDEN_ANGLE){
         vec2 tc = texCoord + vec2(cos(ang), sin(ang)) * uPixelSize * radius;
         vec3 sampleColor = texture2D(image, tc).rgb;
-        float sampleDepth = readDepth(depthMap, tc,uNear,uFar);
-        float sampleSize = getBlurSize(sampleDepth, focusPoint, focusScale);
+        float sampleDepth = readDepth(depthMap, tc, uNear, uFar) / 1000.0;
+        float sampleSize = getCoCSize(sampleDepth, focusDistance, maxCoC);
         if (sampleDepth > centerDepth)
             sampleSize = clamp(sampleSize, 0.0, centerSize*2.0);
         float m = smoothstep(radius-0.5, radius+0.5, sampleSize);
@@ -50,9 +70,19 @@ vec3 depthOfField(vec2 texCoord, float focusPoint, float focusScale) {
 }
 
 void main () {
-    // remap 1..32 to 32..0
-    float focusScale = 32.0 * clamp(1.0 - (uAperture - 1.0) / 32.0, 0.0, 1.0);
-    vec3 color = depthOfField(vTexCoord0, uFocusDistance, focusScale);
+
+    // float F = uFocalLength / 1000.0; // from mm -> m
+    float F = uFocalLength;
+
+    // uAperture is actually F-Stop, so aperture as a diameter is 1 / uAperture
+    float fStop = uAperture;
+    float lensDiameter = 50.0; //mm
+    float A = lensDiameter / fStop;
+    float focusDistance = uFocusDistance / 1000.0; // m -> mm
+    // float maxCoC = A * F / (uFocusDistance - F);
+    float maxCoC = A * F / (focusDistance - F);
+
+    vec3 color = depthOfField(vTexCoord0, focusDistance, maxCoC);
     gl_FragColor = vec4(color, 1.0);
 }
 
