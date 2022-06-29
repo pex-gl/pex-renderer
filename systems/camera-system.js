53310e933717266df896d9c4d0346a4bd944e52f
Marcin Ignac <vorg@Makowka.local>
2022-06-30 00:24:33 +0100

    fix(systems/camera-system): set default camera orbiter position

--- systems/camera-system.js
+++ systems/camera-system.js
@@ -13,6 +13,7 @@ export default function createCameraSystem() {
       if (entity.camera && entity.orbiter) {
         const orbiter = entity.orbiter;
         const camera = entity.camera;
+        //TODO: remove proxy and just use camera as component
         if (!orbiter._orbiter) {
           const proxyCamera = {
             viewMatrix: mat4.create(),
@@ -20,13 +21,18 @@ export default function createCameraSystem() {
             target: camera.target,
             set({ position }) {
               vec3.set(camera.position, position);
+              vec3.set(entity.transform.position, position);
+              entity.transform = {
+                ...entity.transform,
+              };
             },
           };
           orbiter._orbiter = createOrbiter({
             element: entity.orbiter.element || document.body, //TODO: element used to default to ctx.gl.canvas
             autoUpdate: false,
             camera: proxyCamera,
-            distance: 2,
+            position: entity.orbiter.position || [0, 0, 2.5], //TODO: orbiter position vs distance is confusing, shoudln't it come from camera anyway?
+            distance: entity.orbiter.distance || 2,
           });
         } else {
           entity.orbiter._orbiter.updateCamera();
