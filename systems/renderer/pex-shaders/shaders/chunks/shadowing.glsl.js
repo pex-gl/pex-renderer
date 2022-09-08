c24d8ffa3a1a33edf884d7fb982d502570bfcedf
Marcin Ignac <marcin.ignac@gmail.com>
2022-09-08 15:43:44 +0100

    feat(systems/renderer/standard): add soft shadows

--- systems/renderer/pex-shaders/shaders/chunks/shadowing.glsl.js
+++ systems/renderer/pex-shaders/shaders/chunks/shadowing.glsl.js
@@ -47,6 +47,29 @@ export default /* glsl */ `
     return c;
   }
 
+  float rand(float seed) {
+      return fract(sin(seed) * 1231534.9);
+  }
+
+  float rand(vec2 seed) {
+      return rand(dot(seed, vec2(12.9898, 783.233)));
+  }
+
+  //random vector with length 1
+  vec2 rand2(vec2 seed) {
+    const float pi = 3.1415926535897932384626433832795;
+    const float twopi = 2.0 * pi;
+    float r = rand(seed) * twopi;
+    return vec2(cos(r), sin(r));
+  }
+
+  vec2 rotate(vec2 v, float a) {
+    float s = sin(a);
+    float c = cos(a);
+    mat2 m = mat2(c, -s, s, c);
+    return m * v;
+  }
+
   float PCF3x3(sampler2D depths, vec2 size, vec2 uv, float compare, float near, float far){
     float result = 0.0;
     for(int x=-1; x<=1; x++){
@@ -90,6 +113,60 @@ export default /* glsl */ `
     #endif
     return illuminated;
   }
+  float PCF5x5Penumbra(sampler2D depths, vec2 size, vec2 uv, float compare, float near, float far, float radius){
+    float result = 0.0;
+    for(int x=-2; x<=2; x++){
+        for(int y=-2; y<=2; y++){
+            vec2 off = vec2(x,y)/float(size) * radius;
+            off = rotate(off, rand(uv));
+            result += texture2DShadowLerp(depths, size, uv + off, compare, near, far);
+        }
+    }
+    return result/25.0;
+  }
+    vec2 FindBlockers(sampler2D depthMap, vec2 size, vec2 uv, float receiverDepth, float near, float far){
+      float blockers = 0.0;
+      float avgBlockerDistance = 0.0;
+      float result = 0.0;
+      for(int x=-2; x<=2; x++){
+          for(int y=-2; y<=2; y++){
+              vec2 off = vec2(x,y)/float(size) * 10.0;
+              off = rotate(off, rand(uv));
+              off += rand2(uv) * 0.01;
+              float depth = readDepth(depthMap, uv + off, near, far);
+              // if (step(receiverDepth, depth) <= 0.0) {
+              if (depth < receiverDepth) {
+                blockers += 1.0;
+                avgBlockerDistance += depth;
+              }
+          }
+      }
+
+      if (blockers > 0.0) {
+        avgBlockerDistance /= blockers;
+        // return vec2(1.0);
+      }
+      // return vec2(0.0);
+      return vec2(avgBlockerDistance, blockers);
+    }
+    float getSoftShadow(sampler2D depths, vec2 size, vec2 uv, float compare, float near, float far) {
+      if (uv.x < 0.0 || uv.y < 0.0 || uv.x > 1.0 || uv.y > 1.0) {
+        return 1.0;
+      }
+      float receiverDepth = compare;
+      vec2 averageBlocker = FindBlockers(depths, size, uv, receiverDepth, near, far);
+      float averageBlockerDepth = averageBlocker.x;
+
+      float blockerCount = averageBlocker.y;
+      float penumbraSize = 1.0;
+      float lightSize = 3.0;
+
+      if (averageBlockerDepth > 0.0) {
+        penumbraSize = lightSize * (receiverDepth - averageBlockerDepth);
+      }
+      float illuminated = PCF5x5Penumbra(depths, size, uv, compare, near, far, penumbraSize);
+      return illuminated;
+    }
 #endif
 
 #if NUM_POINT_LIGHTS > 0
