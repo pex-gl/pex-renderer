5e399f67a075caedfac2137e6403de7507ea3a26
Marcin Ignac <marcin.ignac@gmail.com>
2022-10-14 16:54:47 +0100

    feat(systems/camera): reimplement orbiter to use entity.transform

--- systems/camera.js
+++ systems/camera.js
@@ -1,4 +1,4 @@
-import { mat4, vec3 } from "pex-math";
+import { mat4, vec3, quat } from "pex-math";
 import { orbiter as createOrbiter } from "pex-cam";
 
 const tmpMatrix = mat4.create();
@@ -8,18 +8,21 @@ export default function createCameraSystem() {
     type: "camera-system",
   };
 
+  const tempMat = mat4.create();
+
   cameraSystem.update = (entities) => {
     for (let entity of entities) {
       if (entity.camera && entity.orbiter) {
         const orbiter = entity.orbiter;
         const camera = entity.camera;
-        //TODO: remove proxy and just use camera as component
+
         if (!orbiter._orbiter) {
           const proxyCamera = {
-            viewMatrix: mat4.create(),
+            viewMatrix: camera.viewMatrix,
             invViewMatrix: camera.invViewMatrix,
-            position: entity.orbiter.position || camera.position,
-            target: camera.target,
+            position: [...entity.transform.position],
+            target: [...orbiter.target],
+            up: [0, 1, 0],
             getViewRay: (x, y, windowWidth, windowHeight) => {
               let nx = (2 * x) / windowWidth - 1;
               let ny = 1 - (2 * y) / windowHeight;
@@ -31,39 +34,39 @@ export default function createCameraSystem() {
               return [[0, 0, 0], vec3.normalize([nx, ny, -camera.near])];
             },
             set({ target, position }) {
+              if (target) {
+                vec3.set(entity.orbiter._orbiter.camera.target, target);
+              }
+
               if (position) {
-                vec3.set(camera.position, position);
+                vec3.set(entity.orbiter._orbiter.camera.position, position);
                 vec3.set(entity.transform.position, position);
               }
-              if (target) {
-                debugger;
-                console.log(target);
-                vec3.set(camera.target, target);
-              }
-              camera.dirty = true;
+
+              mat4.lookAt(
+                tempMat,
+                entity.orbiter._orbiter.camera.position,
+                entity.orbiter._orbiter.camera.target,
+                entity.orbiter._orbiter.camera.up
+              );
+              mat4.invert(tempMat);
+              quat.fromMat4(entity.transform.rotation, tempMat);
+
               entity.transform = {
                 ...entity.transform,
               };
+              camera.dirty = true;
             },
           };
           orbiter._orbiter = createOrbiter({
             element: entity.orbiter.element || document.body, //TODO: element used to default to ctx.gl.canvas
             autoUpdate: false,
             camera: proxyCamera,
-            position: entity.orbiter.position || [0, 0, 2.5], //TODO: orbiter position vs distance is confusing, shoudln't it come from camera anyway?
+            position: proxyCamera.position,
             maxDistance: camera.far * 0.9,
           });
         } else {
           entity.orbiter._orbiter.updateCamera();
-          mat4.lookAt(
-            camera.viewMatrix,
-            camera.position,
-            camera.target,
-            camera.up
-          );
-
-          mat4.set(camera.invViewMatrix, camera.viewMatrix);
-          mat4.invert(camera.invViewMatrix);
         }
 
         if (camera.dirty) {
@@ -75,6 +78,11 @@ export default function createCameraSystem() {
             camera.near,
             camera.far
           );
+
+          mat4.set(camera.invViewMatrix, entity._transform.modelMatrix);
+          //look at matrix is opposite of camera modelMatrix transform
+          mat4.set(camera.viewMatrix, entity._transform.modelMatrix);
+          mat4.invert(camera.viewMatrix);
         }
       }
     }
