f2feab0a63846880dcfd01d3b6684c8e4461bf65
Marcin Ignac <marcin.ignac@gmail.com>
2022-11-06 00:52:40 +0000

    feat(examples/basic-engine): add hooks example

--- examples/basic-engine.js
+++ examples/basic-engine.js
@@ -7,7 +7,14 @@ import {
   entity,
 } from "../index.js";
 import createContext from "pex-context";
-import { cube, torus, sphere } from "primitive-geometry";
+import {
+  cube,
+  torus,
+  sphere,
+  icosphere,
+  plane,
+  cone,
+} from "primitive-geometry";
 import { vec3, quat, mat4, utils } from "pex-math";
 import * as io from "pex-io";
 import createGUI from "pex-gui";
@@ -29,6 +36,7 @@ const {
 
 const ctx = createContext({});
 ctx.gl.getExtension("OES_element_index_uint"); //TEMP
+ctx.gl.getExtension("EXT_color_buffer_float");
 
 window.addEventListener("resize", () => {
   ctx.set({
@@ -185,19 +193,239 @@ function quatFromPointToPointFacingZPos(q, eye, target, up) {
 }
 
 const floorEntity = createEntity({
+  name: "floor",
   transform: transform({
-    position: [0, -1, 0],
+    position: [0, -1.1, 0],
   }),
-  geometry: geometry(cube({ sx: 5, sy: 0.2, sz: 5, nx: 10, ny: 1, nz: 10 })),
+  geometry: geometry(cube({ sx: 15, sy: 0.2, sz: 15, nx: 10, ny: 1, nz: 10 })),
   material: material({
     baseColor: [0, 1, 0, 1],
     metallic: 0,
-    roughness: 0.2,
-    // castShadows: true,
+    roughness: 1.0,
+    castShadows: true,
     receiveShadows: true,
   }),
 });
+floorEntity.geometry.bounds = [
+  [-15, -0.2, -15],
+  [15, 0.2, 15],
+];
 world.add(floorEntity);
+
+//src: https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+const perlinNoiseGLSL = /*glsl*/ `
+// Classic Perlin 3D Noise
+// by Stefan Gustavson
+
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r) {
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec3 P){
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod(Pi0, 289.0);
+  Pi1 = mod(Pi1, 289.0);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 / 7.0;
+  vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 / 7.0;
+  vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+`;
+
+const grassHeight = 1.2;
+const coneGeometry = cone({
+  height: grassHeight,
+  radius: 0.05,
+  nx: 4,
+});
+for (let i = 1; i < coneGeometry.positions.length; i += 3) {
+  coneGeometry.positions[i] += grassHeight / 2;
+}
+
+const grassPlaneGeometry = plane({
+  sx: 5 - 0.05 * 2,
+  sy: 5 - 0.05 * 2,
+  nx: 48,
+  ny: 48,
+  direction: "y",
+  quads: false,
+});
+
+const grassEntity = createEntity({
+  transform: transform({
+    position: [0, -1, 0],
+  }),
+  geometry: geometry({
+    ...coneGeometry,
+    offsets: grassPlaneGeometry.positions,
+    instances: grassPlaneGeometry.positions.length / 3,
+  }),
+  material: material({
+    baseColor: [0.2, 0.8, 0, 1],
+    metallic: 0.2,
+    roughness: 0.6,
+    castShadows: true,
+    receiveShadows: true,
+    hooks: {
+      vert: {
+        DECLARATIONS_END: /*glsl*/ `
+          ${perlinNoiseGLSL}
+          uniform float uTime;
+          varying float vNoiseAmount;
+          varying float vColorNoise;
+        `,
+        BEFORE_TRANSFORM: /*glsl*/ `
+          float noiseAmountX = cnoise(aOffset.xyz * 0.3 + vec3(uTime, 0.0, 0.0));
+          float noiseAmountZ = cnoise(aOffset.xyz * 0.3 + vec3(0.0, 0.0, uTime));
+          float noiseAmountY = 0.7 + 0.7 * cnoise(aOffset.xyz * 0.3 + vec3(1.0, 0.5, 21.520));
+          float y = (position.y + ${grassHeight}/2.0)/${grassHeight};
+          vNoiseAmount = position.y / ${grassHeight};
+          position.x += 0.5 * noiseAmountX * (y * y);
+          position.z += 0.5 * noiseAmountZ * (y * y);
+          position.y *= 1.0 - 2.0 * (0.5 * noiseAmountX * (y * y) * 0.5 * noiseAmountZ * (y * y));
+          position.y *= pow(clamp(length(aOffset.xz/2.0), 0.0, 1.0), 3.0);
+          position.y *= noiseAmountY;
+          vColorNoise = position.y / ${grassHeight};
+        `,
+      },
+      frag: {
+        DECLARATIONS_END: /*glsl*/ `
+        varying float vNoiseAmount;
+        varying float vColorNoise;
+        `,
+        BEFORE_TEXTURES: /*glsl*/ `
+          vec3 dX = dFdx(data.positionView);
+          vec3 dY = dFdy(data.positionView);
+          // data.normalView = normalize(cross(dX, dY));
+          // data.normalWorld = vec3(data.inverseViewMatrix * vec4(data.normalView, 0.0));
+        `,
+        BEFORE_LIGHTING: /*glsl*/ `
+          data.baseColor.rgb = mix(vec3(0.3, 1.0, 0.0), vec3(0.0, 0.5, 0.2), vColorNoise);
+          data.baseColor *= 0.4 + vNoiseAmount;
+          //data.baseColor.g = 0.0;
+        `,
+      },
+      uniforms: (entity) => {
+        return {
+          uTime: (Date.now() % 10000) / 2000,
+        };
+      },
+    },
+  }),
+});
+world.add(grassEntity);
+const sphereEntity = createEntity({
+  name: "sphereEntity",
+  transform: transform({
+    position: [0, 1, 0],
+  }),
+  geometry: geometry(icosphere({ radius: 1, subdivisions: 4 })),
+  material: material({
+    baseColor: [1, 0, 1, 1],
+    metallic: 0,
+    roughness: 0.2,
+    castShadows: true,
+    receiveShadows: true,
+    hooks: {
+      vert: {
+        DECLARATIONS_END: /*glsl*/ `
+          ${perlinNoiseGLSL}
+          uniform float uTime;
+          varying float vNoiseAmount;
+        `,
+        BEFORE_TRANSFORM: /*glsl*/ `
+          vNoiseAmount = 0.5 + 0.5 * cnoise(position.xyz * 2.0 + vec3(uTime, 0.0, 0.0));
+          position.xyz += normal.xyz * vNoiseAmount;
+        `,
+      },
+      frag: {
+        DECLARATIONS_END: /*glsl*/ `
+        varying float vNoiseAmount;
+        `,
+        BEFORE_TEXTURES: /*glsl*/ `
+          vec3 dX = dFdx(data.positionView);
+          vec3 dY = dFdy(data.positionView);
+          data.normalView = normalize(cross(dX, dY));
+          data.normalWorld = vec3(data.inverseViewMatrix * vec4(data.normalView, 0.0));
+        `,
+        BEFORE_LIGHTING: /*glsl*/ `
+          data.roughness = 0.1;
+          data.metallic = step(0.5, vNoiseAmount);
+          data.baseColor = mix(vec3(2.0, 0.4, 0.0), vec3(1.0), data.metallic);
+          // data.baseColor = vec3();
+
+          // data.metallic = 1.0;
+        `,
+      },
+      uniforms: (entity) => {
+        return {
+          uTime: (Date.now() % 1000000) / 1000,
+        };
+      },
+    },
+  }),
+});
+world.add(sphereEntity);
 
 const cameraTargetEntity = createEntity({
   transform: transform({
@@ -242,7 +470,7 @@ const redCubeEntity = createEntity({
     receiveShadows: true,
   }),
 });
-world.add(redCubeEntity);
+// world.add(redCubeEntity);
 
 const greenCubeEntity = createEntity({
   transform: transform({
@@ -257,7 +485,7 @@ const greenCubeEntity = createEntity({
     receiveShadows: true,
   }),
 });
-world.add(greenCubeEntity);
+// world.add(greenCubeEntity);
 
 const blueCubeEntity = createEntity({
   transform: transform({
@@ -272,7 +500,7 @@ const blueCubeEntity = createEntity({
     receiveShadows: true,
   }),
 });
-world.add(blueCubeEntity);
+// world.add(blueCubeEntity);
 
 for (let i = 0; i < 10; i++) {
   const torusEntity = createEntity({
@@ -294,7 +522,7 @@ for (let i = 0; i < 10; i++) {
     }),
     boundingBoxHelper: components.boundingBoxHelper(),
   });
-  world.add(torusEntity);
+  // world.add(torusEntity);
 }
 
 const skyboxEnt = createEntity({
@@ -322,7 +550,7 @@ const directionalLightEntity = createEntity({
   }),
   directionalLight: directionalLight({
     color: [1, 1, 1, 1], //FIXME: instencity is copied to alpha in pex-renderer
-    intensity: 1,
+    intensity: 5,
     castShadows: true,
   }),
   lightHelper: components.lightHelper({ color: [1, 0, 0, 1] }),
@@ -351,7 +579,7 @@ const axis = [
     material: material({ unlit: true, baseColor: color }),
   });
   ent.transform.parent = directionalLightEntity.transform;
-  world.add(ent);
+  // world.add(ent);
 });
 
 const pointLightEntity = createEntity({
@@ -363,9 +591,9 @@ const pointLightEntity = createEntity({
     intensity: 0.2,
     castShadows: true,
   }),
-  lightHelper: components.lightHelper({ color: [1, 0, 0, 1] }),
+  // lightHelper: components.lightHelper({ color: [1, 0, 0, 1] }),
 });
-world.add(pointLightEntity);
+// world.add(pointLightEntity);
 
 const segmentsGeom = {
   positions: [],
@@ -398,7 +626,7 @@ const segmentsEntity = createEntity({
   material: components.material({ type: "segments", baseColor: [1, 1, 1, 1] }),
   transform: components.transform(),
 });
-world.add(segmentsEntity);
+// world.add(segmentsEntity);
 
 async function loadScene() {
   console.log("loadScene loaders.gltf");
@@ -474,6 +702,7 @@ let prevTime = Date.now();
 const renderEngine = createRenderEngine({ ctx });
 
 let pointLightShadowmapPreview;
+let directionalLightShadowmapPreview;
 
 let reflectionProbeTexturePreview = null;
 
@@ -523,10 +752,20 @@ ctx.frame(() => {
   }
 
   if (!pointLightShadowmapPreview) {
-    pointLightShadowmapPreview = gui.addTextureCube(
-      "PointLightShadowmap",
-      pointLightEntity.pointLight._shadowCubemap
-    );
+    pointLightShadowmapPreview = true;
+    // pointLightShadowmapPreview = gui.addTextureCube(
+    //   "PointLightShadowmap",
+    //   pointLightEntity.pointLight._shadowCubemap
+    // );
+    if (
+      directionalLightEntity.directionalLight._shadowMap &&
+      !directionalLightShadowmapPreview
+    ) {
+      directionalLightShadowmapPreview = gui.addTexture2D(
+        "DirectionalLightShadowmap",
+        directionalLightEntity.directionalLight._shadowMap
+      );
+    }
     const standardRenderer = renderEngine.renderers.find(
       (renderer) => renderer.type == "standard-renderer"
     );
