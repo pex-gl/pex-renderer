6bb94a20a0ffec84e30ee4746c43e89fbb0ebf7d
Damien Seguin <dmnsgn@gmail.com>
2023-06-01 13:12:49 +0100

    refactor: remove unused fullscreen quad shader

--- systems/renderer/pex-shaders/shaders/reflection-probe/index.js
+++ systems/renderer/pex-shaders/shaders/reflection-probe/index.js
@@ -2,7 +2,6 @@ import blitToOctMapAtlasFrag from "./blit-to-oct-map-atlas.frag.js";
 import convolveOctMapAtlasToOctMapFrag from "./convolve-oct-map-atlas-to-oct-map.frag.js";
 import cubemapToOctmapFrag from "./cubemap-to-octmap.frag.js";
 import downsampleFromOctMapAtlasFrag from "./downsample-from-oct-map-atlas.frag.js";
-import fullscreenQuadVert from "./fullscreen-quad.vert.js";
 import prefilterFromOctMapAtlasFrag from "./prefilter-from-oct-map-atlas.frag.js";
 
 export default {
@@ -10,6 +9,5 @@ export default {
   convolveOctMapAtlasToOctMap: { frag: convolveOctMapAtlasToOctMapFrag },
   cubemapToOctMap: { frag: cubemapToOctmapFrag },
   downsampleFromOctMapAtlas: { frag: downsampleFromOctMapAtlasFrag },
-  fullscreenQuad: { vert: fullscreenQuadVert },
   prefilterFromOctMapAtlas: { frag: prefilterFromOctMapAtlasFrag },
 };
