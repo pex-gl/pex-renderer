d861c4c120fbe159f4887d0b650099a772799247
Damien Seguin <dmnsgn@gmail.com>
2023-06-05 22:33:43 +0100

    feat: add pex-shaders parser

--- systems/renderer/pex-shaders/index.js
+++ systems/renderer/pex-shaders/index.js
@@ -3,3 +3,5 @@ export { default as pipeline } from "./shaders/pipeline/index.js";
 export { default as postProcessing } from "./shaders/post-processing/index.js";
 export { default as reflectionProbe } from "./shaders/reflection-probe/index.js";
 export { default as skybox } from "./shaders/skybox/index.js";
+
+export { default as parser } from "./parser.js";
