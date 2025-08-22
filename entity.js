073536009709b52696cce077ceaf3283c093fd40
Damien Seguin <dmnsgn@gmail.com>
2025-08-22 16:07:54 +0100

    docs: update entity JSDoc

--- entity.js
+++ entity.js
@@ -1,10 +1,5 @@
 export let entityCount = 0;
 
-/**
- * Create an entity
- * @param {import("./types.js").Entity} [components]
- * @returns {import("./types.js").Entity}
- */
 export default (components = {}) => {
   if (Array.isArray(components)) {
     throw new Error(
